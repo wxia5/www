84f450e764a2127c861bf2824a9ed8f0ea739f44
xwq <xwqhenu2010@163.com>
2018-02-09 17:15:58 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+  	<headMenu></headMenu>
+  	<leftMenu></leftMenu>
+		<div id="cesiumContainer">
+		</div>
+  </div>
+  
+</template>
+
+<script>
+require('../node_modules/cesium/Source/Widgets/widgets.css');
+let Cesium = require('../node_modules/cesium/Source/Cesium.js');
+import headMenu from "./components/headMenu.vue"
+import leftMenu from "./components/leftMenu.vue"
+
+export default {
+	components:{
+		headMenu:headMenu,
+		leftMenu:leftMenu
+	},
+  name: 'App',
+  data(){
+  	return {
+  		
+  	}
+  },
+  mounted:function(){
+  	var viewer = new Cesium.Viewer("cesiumContainer");
+  	    viewer.camera.flyTo({
+    destination : Cesium.Cartesian3.fromDegrees(-117.16, 32.71, 15000.0)
+});
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#cesiumContainer{
+	height: 700px;
+}
+</style>
---
+++ src/components/headMenu.vue
@@ -0,0 +1,47 @@
+<template>
+<div id="header">
+<div class="line"></div>
+<el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1" id="first-item">三维平台</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">地图切换</template>
+    <el-menu-item index="2-1">卫星图</el-menu-item>
+    <el-menu-item index="2-2">矢量图</el-menu-item>
+  </el-submenu>
+  <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">模型加载</a></el-menu-item>
+</el-menu>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '3'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+      	console.log(key)
+        console.log(keyPath);
+      }
+    }
+  }
+</script>
+<style>
+	#header{
+		z-index: 999;
+		/*position: absolute;*/
+	}
+	#first-item{
+		width: 200px;
+	}
+</style>
